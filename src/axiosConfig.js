ded1b88f99390b14a6c5cac78798ad99e5a06b9c
ajuraev <ajuraev20@ku.edu.tr>
2023-07-19 00:47:12 +0300

    api url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://reservations-56ei.onrender.com//',
+    baseURL: 'https://reservations-56ei.onrender.com/',
     headers: {
       'Content-Type': 'application/json',
     },
